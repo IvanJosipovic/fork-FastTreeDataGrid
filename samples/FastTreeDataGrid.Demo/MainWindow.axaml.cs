5ebb32df81f0d86906d909d4389445fef931f287
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-10-27 11:41:23 +0100

    Update MainWindow.axaml.cs

--- samples/FastTreeDataGrid.Demo/MainWindow.axaml.cs
+++ samples/FastTreeDataGrid.Demo/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Rendering;
 using FastTreeDataGrid.Demo.ViewModels;
 
 namespace FastTreeDataGrid.Demo;
@@ -9,6 +10,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        // RendererDiagnostics.DebugOverlays = RendererDebugOverlays.Fps | RendererDebugOverlays.LayoutTimeGraph | RendererDebugOverlays.RenderTimeGraph;
         DataContext = new MainWindowViewModel();
     }
 
